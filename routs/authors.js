ac457ed0790c0c8b2834308f28b5b50ae036a126
xavier <omondix214@gmail.com>
2022-10-03 17:19:32 +0300

    save with mongodb

--- routs/authors.js
+++ routs/authors.js
@@ -12,7 +12,19 @@ rout.get("/new", (req, res) => {
 });
 
 rout.post("/", (req, res) => {
-  res.send(req.body.name);
+  const author = new Author({
+    name: req.body.name,
+  });
+  author.save((err, newAuthor) => {
+    if (err) {
+      res.render("authors/new", {
+        author: author,
+        errorMessage: "Error creating Author",
+      });
+    } else {
+      res.redirect(`authors`);
+    }
+  });
 });
 
 module.exports = rout;
